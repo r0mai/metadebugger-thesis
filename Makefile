11e1654c2cc1b1975bbbc0d2de5708bd1b204244
R0mai <r0maikx02b@gmail.com>
2014-10-18 11:13:23 +0200

    Add png to forwardtrace algorithm

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
 
 NAME=thesis
 TEXSRCS=$(wildcard tex/*.tex)
+PNGS=$(wildcard img/*.png)
+EPSS=$(patsubst %.png,%.eps,$(PNGS))
+
+img/%.eps: img/%.png
+	convert $< $@
 
 .PHONY: my_all
-my_all: pdf
+my_all: $(EPSS) pdf
 
 include /usr/local/share/latex-mk/latex.gmk
 
--- src/fibonacci.hpp
+++ src/fibonacci.hpp
@@ -1,5 +1,7 @@
 template<int N> struct fib {
-    static const int value = fib<N - 2>::value + fib<N - 1>::value;
+    static const int value =
+        fib<N - 2>::value +
+        fib<N - 1>::value;
 };
 template<> struct fib<1> { static const int value = 1; };
 template<> struct fib<0> { static const int value = 0; };
