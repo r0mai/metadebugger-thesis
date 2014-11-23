6dba827c285f00be4a21ea131ee779b279fd15e6
R0mai <r0maikx02b@gmail.com>
2014-11-23 16:32:16 +0100

    Add Getting Started

---
+++ src/fibonacci_as_msh_input.hpp
@@ -0,0 +1,8 @@
+> template<int N> struct fib { \
+...>static const int value = \
+...>   fib<N - 2>::value + \
+...>   fib<N - 1>::value; \
+...>};
+> template<> struct fib<1> { static const int value = 1; };
+> template<> struct fib<0> { static const int value = 0; };
+> template<int N> struct int_ {};
