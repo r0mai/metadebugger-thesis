24575b02662a5802c70039eb641b87232406aa98
Andras Kucsma <r0maikx02b@gmail.com>
2014-11-24 10:27:04 +0100

    Use another code style for mdb input/output

--- src/fibonacci_as_msh_input.hpp
+++
@@ -1,8 +0,0 @@
-> template<int N> struct fib { \
-...>static const int value = \
-...>   fib<N - 2>::value + \
-...>   fib<N - 1>::value; \
-...>};
-> template<> struct fib<1> { static const int value = 1; };
-> template<> struct fib<0> { static const int value = 0; };
-> template<int N> struct int_ {};
