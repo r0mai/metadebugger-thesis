72793f7dea1cd043c23aa37be068aff1cfabaa62
R0mai <r0maikx02b@gmail.com>
2014-11-29 12:50:43 +0100

    Add typedefs to rollback_t example code

--- src/metaprogram_rollback_t.hpp
+++ src/metaprogram_rollback_t.hpp
@@ -1,6 +1,9 @@
 class metaprogram {
   // ...
 public:
+  typedef boost::optional<vertex_descriptor> optional_vertex_descriptor;
+  typedef boost::optional<edge_descriptor> optional_edge_descriptor;
+
   struct step_rollback_t {
     optional_edge_descriptor popped_edge;
     optional_vertex_descriptor discovered_vertex;
